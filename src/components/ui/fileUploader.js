e6d5db1dfecbd3cd2375f633b71aba15cde3a112
1stmuse <olysegs@gmail.com>
2021-01-13 03:05:15 +0100

    made the image show

--- src/components/ui/fileUploader.js
+++ src/components/ui/fileUploader.js
@@ -23,15 +23,17 @@ export default class Fileuploader extends Component {
         })
     }
 
-    handleUploadSuccess =(filename) =>{
-        console.log(filename)
+    handleUploadSuccess =(filename, task) =>{
+        let name =task._snapshot.ref._delegate._location.path_
+         let usename = name.split('/')[1]
+
         this.setState({
-            name:filename,
+            name:usename,
             isUploading:false
         })
 
         firebase.storage().ref(this.props.dir)
-        .child(filename).getDownloadURL()
+        .child(usename).getDownloadURL()
         .then(url=>{
             console.log(url)
             this.setState({fileURL: url})
@@ -59,10 +61,11 @@ export default class Fileuploader extends Component {
                         accept = "image/*"
                         name="image"
                         randomizeFilename
-                        storageRef={firebase.storage().ref(this.props.dir)}
+                        storageRef={firebase.storage().ref('players')}
                         onUploadStart={this.handleUploadStart}
                         onUploadError={ this.handleUploadError}
                         onUploadSuccess= { this.handleUploadSuccess}
+                        metadata={{name: 'image'}}
                       />
                   </div>
                   : null
@@ -77,6 +80,18 @@ export default class Fileuploader extends Component {
                     </div>
                     : null
                  }
+                 {this.state.fileURL ?
+                    <div className='image_upload_container'>
+                        <img 
+                            style={{
+                                width:'100%'
+                            }}
+                            src={this.state.fileURL}
+                            alt={this.state.name}
+                        />
+                    </div>
+                    :null
+                }
             </div>
         )
     }
